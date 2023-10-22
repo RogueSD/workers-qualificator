0bc4d00f2bd99f379c5d735e260e20bc22270da1
haskinthg <80718822+haskinthg@users.noreply.github.com>
2023-10-22 18:48:20 +0300

    1С integration (#3)

--- src/main/java/edu/vgtu/project/controller/IntegrationController.java
+++ src/main/java/edu/vgtu/project/controller/IntegrationController.java
@@ -1,8 +1,9 @@
 package edu.vgtu.project.controller;
 
+import edu.vgtu.project.service.IntegrationService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -11,21 +12,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileInputStream;
-import java.nio.charset.StandardCharsets;
-
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/integration")
 public class IntegrationController {
+
+    private final IntegrationService integrationService;
     @GetMapping("/export")
     public ResponseEntity<Resource> export() {
         log.info("Получен запрос экспорта файла для системы 1С.");
-        ByteArrayResource resource = new ByteArrayResource("test".getBytes(StandardCharsets.UTF_8));
-        log.info("Возрващение заглушки");
+        ByteArrayResource resource = integrationService.getReport();
         return ResponseEntity.ok()
                 .contentLength(resource.contentLength())
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=test.txt")
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=export.xlsx")
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .body(resource);
     }
